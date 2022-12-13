838a2aa41a18ea98097f5151922f53b92b9bb48b
parkmoen <pmj6516@naver.com>
2022-12-14 04:25:25 +0900

    Feat: Loading Component 기능 구현 & 비동기 로직에 Loading 연결

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div ref="triggerElement">
     <ul class="movie-list">
+      <Spinner
+        class="spinner"
+        :size="30"
+        :width="5"
+        :loading="keywordStore.loading" />
       <MovieItem
-        v-for="movieItem in keywordStore.movieInformation"
+        v-for="movieItem in keywordStore.movieInformation[0]"
         :key="movieItem.imdbID"
         :movie-item="movieItem" />
     </ul>
+    <!-- <Observer @intersect="intersected" /> -->
   </div>
 </template>
 
@@ -13,16 +19,26 @@
 import { mapStores } from 'pinia';
 import { useKeywordStore } from '~/store/keyword.js';
 
-import MovieItem from './MovieItem';
+import MovieItem from '~/components/MovieItem';
+import Spinner from '~/components/Spinner';
+// import Observer from '~/components/Observer';
 
 export default {
   components: {
     MovieItem,
+    Spinner,
+    // Observer,
   },
 
   computed: {
-    ...mapStores(useKeywordStore)
-  },  
+    ...mapStores(useKeywordStore),
+  },
+
+  // methods: {
+  //   async intersected() {
+  //     this.keywordStore.fetchKeyword(this.keywordStore.keyword);
+  //   },
+  // },
 };
 </script>
 
@@ -33,5 +49,11 @@ export default {
   flex-wrap: wrap;
   justify-content: center;
   padding-bottom: 50px;
+  position: relative;
+
+  .spinner {
+    position: absolute;
+    top: 400%;
+  }
 }
 </style>
---
+++ src/components/Spinner.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    v-if="loading"
+    :style="{
+      width: `${size}px`,
+      height: `${size}px`,
+      padding,
+    }"
+    :class="{
+      absolute,
+      fixed,
+    }"
+    class="loader-wrapper">
+    <div
+      :style="{
+        borderWidth: `${width}px`,
+      }"
+      class="loader"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: true,
+    },
+    padding: {
+      type: String,
+      default: '0',
+    },
+    size: {
+      type: Number,
+      default: 22,
+    },
+    width: {
+      type: Number,
+      default: 3,
+    },
+    absolute: {
+      type: Boolean,
+      default: false,
+    },
+    fixed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.loader-wrapper {
+  flex-shrink: 0;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  &.absolute {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+
+  &.fixed {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+}
+.loader {
+  width: 100%;
+  height: 100%;
+  flex-shrink: 0;
+  border-style: solid;
+  border-radius: 50%;
+  border-top-color: rgba(black, 0.1) !important;
+  border-color: $color-log-background;
+  box-sizing: border-box;
+  animation: spin 1.5s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
